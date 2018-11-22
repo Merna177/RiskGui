a4a8706fd01fa2df04ee150b39dcab6b0a3b3308
Merna177 <ronazakaria@gmail.com>
2018-11-22 14:35:16 +0200

    intial gui

---
+++ trial/src/application/Main.java
@@ -0,0 +1,43 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+
+public class Main extends Application {
+	private Stage primaryStage;
+	private Pane mainLayOut ;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+		this.primaryStage=primaryStage;
+		this.primaryStage.setTitle("RISK");
+		ShowMainView();		
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	private void ShowMainView() {
+		FXMLLoader loader= new FXMLLoader();
+		loader.setLocation(Main.class.getResource("Sample.fxml"));
+		try {
+			mainLayOut = loader.load();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ trial/src/application/SampleController.java
@@ -0,0 +1,15 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class SampleController {
+	@FXML
+    private Button sim;
+
+    @FXML
+    void simulationClicked(ActionEvent event) {
+              
+    }	
+}
